70f058d8eaeafbbe9a5a9f20b4a685c2f1793678
dantecostamagna <dantecostamagna@gmail.com>
2023-02-25 11:13:55 -0300

    Finalizado

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.set('puerto', process.env.PORT || 3001);
+app.set('puerto', process.env.PORT || 3000);
 
-app.listen(app.get('puerto'), () => console.log("Servidor corriendo"));
+const port = process.env.PORT || 3000;
+app.listen(port,() =>
+    console.log("Servidor corriendo en el puerto http://localhost:" + port)
+);
 
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/home.html"));
